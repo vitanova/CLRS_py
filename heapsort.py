10cec13e6bd79ea8f1eedf50bc2dc1fec23eb085
vita nova <vitanova1001@gmail.com>
2021-02-01 12:48:06 -0600

    heapsort1

--- heapsort.py
+++ heapsort.py
@@ -20,7 +20,7 @@ def max_heapify(A, i):
         
 def build_max_heap(A):
     n = len(A)
-    for i in range(1, int(n/2)+1)[::-1]:
+    for i in range(1, Parent(n)+1)[::-1]:
         max_heapify(A, i)
 
 def heapsort(A):
@@ -28,12 +28,10 @@ def heapsort(A):
     n = len(A)
     A_s = [0]*n
     for i in range(1, n+1)[::-1]:
-        temp = A[1-1]
-        A[1-1] = A[i-1]
-        A[i-1] = temp
-        A_s[i-1] = A[i-1]
-        A = A[:(i-1)]
-        max_heapify(A, 1)
+    	A_s[i-1] = A[1-1]
+    	A[1-1]   = A[i-1]
+    	A = A[:(i-1)]
+    	max_heapify(A, 1)
     return A_s
 
 A = [2, 8, 7, 1, 3, 5, 6, 4]
